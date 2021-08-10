b204715919e2d86335ebe5dc2ed6aee5e784ccc3
SIGUI Kessé Emmanuel (https://sikessem.com) <contact@sikessem.com>
2021-08-10 15:03:02 +0200

    Create the render class

---
+++ lib/Render.php
@@ -0,0 +1,31 @@
+<?php namespace Archidoc;
+
+class Render {
+
+	public function __construct(protected string $content) {}
+	
+	protected string $tag = '';
+
+	protected array $attributes = [];
+
+	protected array $properties = [];
+
+	protected array $events = [];
+
+	public function is(Widget $widget): self {
+		$this->tag = $widget->tag();
+		$this->attributes = $widget->attributes();
+		return $this;
+	}
+
+	public function has(Design $design) {
+		$rule = $design->rule();
+		foreach($this->$widget->properties() as $name => $value)
+			$this->$properties[$rule][$name] = $value;
+		return $this;
+	}
+
+	public function uses(Action $action) {
+		$this->events[$action->event()][] = $action->callback();
+	}
+}
