9b22f44235507b122ffdf256ad8e9d67efccd7ca
SIGUI Kessé Emmanuel (https://sikessem.com) <contact@sikessem.com>
2021-08-10 15:19:36 +0200

    Create the widget class

---
+++ lib/Widget.php
@@ -0,0 +1,48 @@
+<?php namespace Archidoc;
+
+class Widget {
+	
+	protected string $tag;
+
+	protected array $attributes = [];
+
+	public function __construct(string $tag, array $attributes) {
+		$this->setTag($tag);
+		$this->setAttributes($attributes);
+	}
+
+	public function setTag(string $tag) {
+		$this->valid_name($tag);
+		$this->tag = $tag;
+	}
+
+	public function tag(): string {
+		return $this->tag;
+	}
+
+	public function setAttributes(array $attributes) {
+		foreach($attributes as $name => $value)
+			$this->setAttribute($name, $value);
+	}
+
+	public function setAttribute(string $name, mixed $value) {
+		$this->valid_name($name);
+		$this->attributes[$name] = $value;
+	}
+
+	public function attributes(): array {
+		return $this->attributes;
+	}
+
+	public function attribute(string $name): mixed {
+		return $this->attributes[$name] ?? null;
+	}
+
+	protected function valid_name(string $name): void {
+		if(empty($tag)) 
+			throw Error('Empty tag given', Error::EMPTY_VALUE);
+
+		if(!preg_match('/^[^0-9[^a-zA-Z-]]+[\w-]*$/', $tag))
+			throw Error("Invalid tag ($tag) given". Error::INVALID_VALUE);
+	}
+}
