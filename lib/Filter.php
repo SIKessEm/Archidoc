a8a6574641346133cc546a0904b720a0111d4026
SIGUI Kessé Emmanuel (https://sikessem.com) <contact@sikessem.com>
2021-08-31 15:11:08 +0200

    Create the filter class

---
+++ lib/Filter.php
@@ -0,0 +1,14 @@
+<?php namespace Archidoc;
+
+class Filter {
+
+	static function satitize(string $id): string {
+		
+		return strtolower(trim($id));
+	}
+
+	static function validate(string $id): bool {
+		
+		return !empty($id) && preg_match('/^[^0-9[^a-zA-Z-]]+[\w-]*$/', $id);
+	}
+}
