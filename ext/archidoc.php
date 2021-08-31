3f4b7de75d9832930fbe92edb61b3d3f4f01ccba
SIGUI Kessé Emmanuel (https://sikessem.com) <contact@sikessem.com>
2021-08-31 15:35:56 +0200

    Add classes helpers from /ext/archidoc.php

--- ext/archidoc.php
+++ ext/archidoc.php
@@ -1,1 +1,21 @@
-<?php namespace Archidoc;
+<?php namespace SIKessEm\Archidoc;
+
+function render(string $content): Render {
+
+  return new Render($content);
+}
+
+function widget(string $tag, array $attributes): Widget {
+
+  return new Widget($tag, $attributes);
+}
+
+function design(string $rule, array $properties): Design {
+
+  return new Design($rule, $properties);
+}
+
+function action(string $event, callable $callback): Action {
+
+  return new Action($event, $callback);
+}
